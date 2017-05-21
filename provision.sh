b29fb80b44a2f4ccd741012df4fe77ec57a1177c
gomesuit <gomesuit@gmail.com>
2017-05-22 01:45:10 +0900

    save

--- provision.sh
+++ provision.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 set -e
 
+sudo yum install -y vim
 
+sudo yum install -y iptables-services
+sudo systemctl stop firewalld
+sudo systemctl mask firewalld
+sudo systemctl start iptables
+sudo systemctl enable iptables
